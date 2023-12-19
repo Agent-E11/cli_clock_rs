e745f79da0b865f237b4b704796629692be31007
Eamon <eamon.r.burns@gmail.com>
2023-12-19 11:23:11 -0800

    Only render and print time if changed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// use cli_clock::Digit::*;
 use std::{thread, time::Duration};
 use chrono::Local;
 use cli_clock::BlockChar3By5;
@@ -6,16 +5,27 @@ use cli_clock::BlockChar3By5;
 fn main() {
     let filled_cell = ['█', '█'];
     let blank_cell = [' ', ' '];
+    const WAIT_TIME: Duration = Duration::from_millis(50);
+
+    let mut rendered_time = Local::now();
 
     loop {
+
         let current_time = Local::now();
-        let curr_h: [[[bool; 3]; 5]; 2] = current_time.format("%H").to_string()
+        if rendered_time.format("%H%M%S").to_string() == current_time.format("%H%M%S").to_string() {
+            thread::sleep(WAIT_TIME);
+            continue;
+        }
+
+        rendered_time = current_time;
+
+        let curr_h: [[[bool; 3]; 5]; 2] = rendered_time.format("%H").to_string()
             .chars().map(|c| c.char_grid())
             .collect::<Vec<_>>().try_into().unwrap();
-        let curr_m: [[[bool; 3]; 5]; 2] = current_time.format("%M").to_string()
+        let curr_m: [[[bool; 3]; 5]; 2] = rendered_time.format("%M").to_string()
             .chars().map(|c| c.char_grid())
             .collect::<Vec<_>>().try_into().unwrap();
-        let curr_s: [[[bool; 3]; 5]; 2] = current_time.format("%S").to_string()
+        let curr_s: [[[bool; 3]; 5]; 2] = rendered_time.format("%S").to_string()
             .chars().map(|c| c.char_grid())
             .collect::<Vec<_>>().try_into().unwrap();
 
@@ -60,6 +70,6 @@ fn main() {
         }
 
         print!("{esc}[2J{esc}[1;1H{output}", esc = 27 as char); // Clear screen and print clock
-        thread::sleep(Duration::from_millis(50));
+        thread::sleep(WAIT_TIME);
     }
 }
